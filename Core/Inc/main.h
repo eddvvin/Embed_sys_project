3b00f25a8f0ea4f7974b91296d26bff928c27507
eddvvin <71153825+eddvvin@users.noreply.github.com>
2025-12-03 14:35:18 -0600

    Update main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -54,11 +54,18 @@ void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
+
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define BTN_ALL_Pin        GPIO_PIN_0
+#define BTN_ALL_GPIO_Port  GPIOC
+#define BTN_L1_Pin         GPIO_PIN_1
+#define BTN_L1_GPIO_Port   GPIOC
+#define BTN_L2_Pin         GPIO_PIN_2
+#define BTN_L2_GPIO_Port   GPIOC
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
